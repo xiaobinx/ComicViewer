fa797047c68a7fa8f106cd1e5bbffeb2ee4e1065
xiaobin <xiaobinh@hotmail.com>
2019-01-02 02:45:53 +0800

    代码优化

--- app/src/main/java/com/bq/androidx/tool/MainHandler.kt
+++ app/src/main/java/com/bq/androidx/tool/MainHandler.kt
@@ -3,4 +3,12 @@ package com.bq.androidx.tool
 import android.os.Handler
 import android.os.Looper
 
-object MainHandler : Handler(Looper.getMainLooper())
+object MainHandler : Handler(Looper.getMainLooper()) {
+    fun runOnUiThread(action: Runnable) {
+        if (Looper.myLooper() == Looper.getMainLooper()) {
+            action.run()
+        } else {
+            post(action)
+        }
+    }
+}
